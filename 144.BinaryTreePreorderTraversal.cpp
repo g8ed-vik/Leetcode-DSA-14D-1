06d240a9734729dbf63f385ecb962a525b62a1a3
Souvik Pal <84220139+g8ed-vik@users.noreply.github.com>
2022-08-31 07:42:27 +0530

    Create 144.BinaryTreePreorderTraversal.cpp

---
+++ 144.BinaryTreePreorderTraversal.cpp
@@ -0,0 +1,67 @@
+/*
+Given the root of a binary tree, return the preorder traversal of its nodes' values.
+
+ 
+
+Example 1:
+
+
+Input: root = [1,null,2,3]
+Output: [1,2,3]
+Example 2:
+
+Input: root = []
+Output: []
+Example 3:
+
+Input: root = [1]
+Output: [1]
+ 
+
+Constraints:
+
+The number of nodes in the tree is in the range [0, 100].
+-100 <= Node.val <= 100
+*/
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> preorderTraversal(TreeNode* root) {
+         vector<int> ans;
+        if(!root) return ans;
+        
+        stack<TreeNode*> stk;
+        
+        stk.push(root);
+        
+        TreeNode* cur;
+        
+        while(!stk.empty()){
+            cur = stk.top(); stk.pop();
+            
+            ans.emplace_back(cur->val);
+            if(cur->right) stk.push(cur->right);
+            if(cur->left) stk.push(cur->left);
+        }
+        
+        return ans;
+    }
+};
+
+/*
+69 / 69 test cases passed.
+Status: Accepted
+Runtime: 6 ms
+Memory Usage: 8.5 MB
+*/
